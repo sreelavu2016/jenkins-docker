188d24d14cf377afe25cdcc6637a9f539d5d651a
sreelavu2016 <sreenu.soft2012@gmail.com>
2020-11-16 12:23:53 +0530

    Update SampleController.java

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class SampleController {
 	
+	@GetMapping("/")
 	public String sayGreetings() {
 		return "Hello Spring boot docker CI/CD PipeLine";
 	}
